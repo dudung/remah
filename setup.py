2c0d00746167a77dff2fcf6e9161ab45845a7cfd
Sparisoma Viridi <dudung@gmail.com>
2024-06-29 17:32:09 +0700

    restructure folder

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='remah',
     version='0.0.5',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(where='src/remah/python'),
+    package_dir={'': 'src/remah/python'},
     install_requires=[
         # Your dependencies
     ],
@@ -24,4 +24,10 @@ setup(
     description='python package for mixed modeling approaches',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
 )
