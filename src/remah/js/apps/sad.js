5dd5cde71617f78e10b739de13c5f093276f5f86
Sparisoma Viridi <dudung@gmail.com>
2024-07-02 06:24:13 +0700

    update

--- src/remah/js/apps/sad.js
+++ src/remah/js/apps/sad.js
@@ -6,6 +6,8 @@
   Sparisoma Viridi | https://github.com/dudung
   
   # Notes
+  20240702 Continue developing the app.
+  
   20240701 Restart first app on Remah.
   - Discuss the idea with GPT-4o and accept the suggested name [1].
   - Try to make it as simple as possbile with least number of elements.
@@ -17,12 +19,45 @@
 */
 
 
-// Execute main function
+// Execute main function.
 main();
 
 
-// Define main function
+// Define main function.
 function main() {
+  const el = createUI()
+  loadDefaultParamsTo(el.ta);
+  
+  el.btn.addEventListener('click',
+    (e) => {
+      //let params = getParamsFromText(label, el.ta.value);
+    }
+  );
+}
+
+
+// Load default parameters to textarea as input element.
+function loadDefaultParamsTo(ta) {
+  var text = ""
+  text += "60\n"
+  text += ".10 .10 .10\n"
+  text += ".10 .00 .10\n"
+  text += ".10 .10 .10\n"
+  text += "\n"
+  text += "20\n"
+  text += ".05 .05 .05\n"
+  text += ".10 .05 .10\n"
+  text += ".10 .20 .10\n"
+  text += "\n"
+  text += "20\n"
+  text += ".10 .20 .10\n"
+  text += ".10 .05 .10\n"
+  text += ".05 .05 .05\n"
+  ta.value = text;
+}
+
+// Create ui and return the interactive elements.
+function createUI() {
   let appDiv = document.createElement('div');
   with(appDiv.style) {
     border = '1px solid #eee';
@@ -51,6 +86,7 @@ function main() {
     overflowY = 'scroll';
     marginLeft = '1px';
     marginBottom = '-3px';
+    fontSize = '10px';
   }
   
   let btn = document.createElement('button');
@@ -59,9 +95,17 @@ function main() {
     width = '97px';
     marginLeft = '1px';
   }
-  
+    
   document.body.appendChild(appDiv);
     appDiv.appendChild(can);
     appDiv.appendChild(ta);
     appDiv.appendChild(btn);
+  
+  let elems = {
+    can: can,
+    ta: ta,
+    btn: btn,
+  }
+  
+  return elems;
 }
