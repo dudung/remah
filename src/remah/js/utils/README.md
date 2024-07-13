8ad2cf3060738650a4729d99f7762bc133800064
Sparisoma Viridi <dudung@gmail.com>
2024-07-13 09:22:19 +0700

    new in utils

---
+++ src/remah/js/utils/README.md
@@ -0,0 +1,4 @@
+# apps
+Some general-purpose utilities
+
++ [draw-matrix.js](draw-matrix.js) Draw a matrix on a canvas.
---
+++ src/remah/js/utils/draw-matrix.js
@@ -0,0 +1,19 @@
+/*
+  draw-matrix.js
+  Draw matrix on canvas
+  v0.0.1 - 20240713
+
+  Sparisoma Viridi | https://github.com/dudung
+  
+  # Notes
+  20240713 Start this utility.
+*/
+
+
+// Execute main function.
+main();
+
+
+// Define main function.
+function main() {
+}
