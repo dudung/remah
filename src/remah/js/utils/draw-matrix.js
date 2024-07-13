a2e09f1cc11c9013dce398a509faf67dbfdc5d9f
Sparisoma Viridi <dudung@gmail.com>
2024-07-13 10:49:41 +0700

    update draw matrix

--- src/remah/js/utils/draw-matrix.js
+++ src/remah/js/utils/draw-matrix.js
@@ -7,7 +7,8 @@
   
   # Notes
   20240713 Start this utility.
-  - Chose kebab-case over snake_case, camelCase of PascalCase.
+  - Choose kebab-case over snake_case, camelCase of PascalCase.
+  - Draw matrix on canvas with only black and white colors.
 */
 
 
@@ -17,4 +18,54 @@ main();
 
 // Define main function.
 function main() {
+  let c = createCanvas(400, 200);
+  let m = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  ]
+    
+  drawMatrixBW(m, c);
+}
+
+
+// Draw matrix on canvas with black and white colors
+function drawMatrixBW(matrix, canvas) {
+  let ctx = canvas.getContext('2d');
+  
+  let w = canvas.width / matrix[0].length;
+  let h = canvas.height / matrix.length;
+  
+  console.log(w, h)
+  
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      ctx.fillStyle = matrix[i][j] ? 'black' : 'white';
+      ctx.strokeStyle = matrix[i][j] ? 'white' : 'black';
+      ctx.lineWidth = 0.5;
+      
+      ctx.beginPath()
+      ctx.rect(j * w, i * h, w, h);
+      ctx.fill();
+      ctx.stroke()
+    }
+  }
+}
+ 
+
+// Draw canvas and append it to document.body
+function createCanvas(width, height) {
+  let can = document.createElement('canvas');
+  can.width = width;
+  can.height = height;
+  can.style.width = width + 'px';
+  can.style.height = height + 'px';
+  can.style.border = '1px solid #ccc';
+  document.body.appendChild(can);
+  return can;
 }
